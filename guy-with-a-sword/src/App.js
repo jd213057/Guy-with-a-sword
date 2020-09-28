4e65df70437b52314e286a45b8417e2ed0705b91
jd213057 <jonathan.dimur@gmail.com>
2020-09-28 21:29:00 +0200

    v.1.0.0.1 : check pxi working

--- guy-with-a-sword/src/App.js
+++ guy-with-a-sword/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+
+const pixi = require("pixi.js")
 
 function App() {
+  pixi.utils.sayHello();
   return (
     <div className="App">
       <header className="App-header">
